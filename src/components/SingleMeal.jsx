8ee5c5d08544b7b453acdf4445bca186f36256da
Altamash-Khan-2812 <altamashkhn281@gmail.com>
2025-09-04 00:45:54 +0530

    completed styling for today's special page

--- src/components/SingleMeal.jsx
+++ src/components/SingleMeal.jsx
@@ -1,8 +1,10 @@
 import { useLocation } from "react-router-dom";
 import BackButton from "./BackButton";
+import { useState } from "react";
 
 const SingleMeal = () => {
   const location = useLocation();
+  const [expanded, setExpanded] = useState(false);
   const meal = location.state;
   console.log(meal);
 
@@ -11,7 +13,7 @@ const SingleMeal = () => {
       <BackButton />
       <div className="flex gap-8 mt-10">
         <img src={meal.strMealThumb} className="rounded-2xl w-[50%] h-full" />
-        <div className="w-[50%]">
+        <div className="w-[50%] flex flex-col justify-between">
           <h2 className="text-4xl font-semibold font-[inter] text-gray-900 mb-4">
             {meal.strMeal}
           </h2>
@@ -24,11 +26,18 @@ const SingleMeal = () => {
             </p>
           </div>
           <p className=" text-lg text-gray-700 font-[inter] mb-10">
-            A delicious {meal.strMeal} recipe that you will love!
+            A delicious {meal.strMeal.toLowerCase()} recipe that you will love!
           </p>
-          <div className="border-1 border-t-0 border-gray-200 px-5">
-            <p className="text-3xl font-semibold text-gray-900 border-b-1">Instructions</p>
-            <p>{meal.strInstructions.slice(0, 400)}</p>
+          <div className="border-1 border-t-0 bg-white border-gray-200 p-5 pt-0 rounded-b-lg">
+            <p className="text-3xl font-semibold text-gray-900 border-b-1 pb-2 mb-5">
+              Instructions
+            </p>
+            <p className="text-lg leading-normal text-gray-700 mb-2">
+              {expanded ? meal.strInstructions : `${meal.strInstructions.substring(0, 300)}...`}
+            </p>
+            <button className="text-blue-600 text-lg hover:underline w-full text-left hover:text-blue-800 cursor-pointer focus:outline-none border-b-1 pb-7" onClick={() => setExpanded(!expanded)}>{expanded ? 'Read Less' : 'Read More'}</button>
+            <p className="text-xl text-gray-800 pt-5 mb-3">Source</p>
+            <a href={meal.strSource} className="text-orange-600 hover:text-orange-700 hover:underline">{meal.strSource}</a>
           </div>
         </div>
       </div>
